aa0e58a4382981a727cd7435c013a872d3f6e22a
AlhichriMaster <g6youssuf@gmail.com>
2024-09-28 15:25:44 -0400

    inbox and root images

--- src/components/inbox/inbox.jsx
+++ src/components/inbox/inbox.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import { useAuth } from '../../contexts/authContext/authContext'
+import React, {useState, useEffect } from 'react'
+import { useAuth } from '../../contexts/authContext/authContext';
+
+import { db } from '../../firebase/firebase';
+import { doc, getDoc} from "firebase/firestore";
+
+
 
 const Inbox = () => {
-    const { currentUser } = useAuth()
-    return (
-        <div className='text-2xl font-bold pt-14'>Hello</div>
-    )
-}
+    const { currentUser } = useAuth();
+    const [showNotification, setShowNotification] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const checkReportsDoc = async () => {
+        try {
+          if (currentUser) {
+            // console.log(currentUser.uid);
+            const reportsDocRef = doc(db, "report_form_join", currentUser.uid);
+            const repDoc = await getDoc(reportsDocRef);
+            // console.log(repDoc.id);
 
-export default Inbox
+            if (repDoc.id) {
+                const reportData = repDoc.data(); // Get document data
+                if (reportData.status === false) {
+                  setShowNotification(true);
+                }
+            }
+          }
+        } catch (err) {
+          setError("Failed to fetch reports. Please try again later.");
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      checkReportsDoc();
+    }, [currentUser]);
+  
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+  
+    return (
+      <div>
+        {error && <div className="error">{error}</div>}
+        {showNotification && (
+          <div className="notification">
+            You have new reports to view!
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default Inbox;
